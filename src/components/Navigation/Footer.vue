78d87447bf70c931d3ba7d8b07f757278e921857
小赵 <979876040@qq.com>
2020-12-26 09:25:11 +0800

    zyd

---
+++ src/components/Navigation/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="foot">
+    <van-tabbar v-model="active" placeholder  @change="changeTab">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">分类</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的易果</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import { Tabbar, TabbarItem } from "vant";
+
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+export default {
+  data() {
+    return {
+      active: 0,
+      url: ["/frist", "/list/twolist", "/cart","/center"],
+      
+    };
+  },
+   created() {
+        this.active = this.url.indexOf(this.$route.path);
+    },
+    methods: {
+        changeTab(index) {
+            this.$router.push(this.url[index]);
+            // console.log(index);
+        },
+    },
+    watch: {
+        $route: function(val){
+            
+            this.active = this.url.indexOf(val.fullPath);
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.foot{
+  height: 50px;
+}
+
+</style>
